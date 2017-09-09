b98c2de4c6d80671d75f0607c752d562922af4a0
Goutham Deva <godeva@wpi.edu>
2017-09-08 23:41:00 -0400

    LandingPage Basic Navigation
    Created a prototype landing page for us to use which we can potentially and add into it. Used angular, added js, css styling, javascript files, placeholder assests and html.

---
+++ landingPage/js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// optional controllers
+function HomeCtrl($scope, $http) {
+}
+
+function ProjectCtrl($scope, $http) {
+}
+
+function AboutCtrl($scope, $http, $timeout) {
+}
+
+function ContactCtrl($scope, $http, $timeout) {
+}
