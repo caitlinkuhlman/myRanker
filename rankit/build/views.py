5794691cec79ca55f25dba85163c7fdd434a3830
Malika <alinurbe@gmail.com>
2017-10-26 15:39:58 -0400

    datasets

--- rankit/build/views.py
+++ rankit/build/views.py
@@ -5,10 +5,21 @@ build_blueprint = Blueprint(
     template_folder='templates'
 )
 
+@build_blueprint.route('build/dataset', methods=["GET"])
+def processDataset():
+    dataset_name = request.args.lists()
+
+    # dataset_name.get('dataset_name', default=-1, type="")
+    dataset_name = list(dataset_name)
+    # dataset_name = dataset_name.get('dataset_name', default=-1, type="")
+
+    print("Dataset : %s  " % dataset_name)
+    return dataset_name
 
 @build_blueprint.route('/build/submit', methods=["POST"])
 def build():
-    request.form
     print("ranking...")
-    #rank = build_rank.build(dataset=dataset, pairsfile=pairsfile)
+    # rank = build_rank.build(dataset=dataset_name, pairsfile=pairsfile)
+    # return rank
+
 
