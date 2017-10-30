e3ae5734edda275a01ab1fc39c8785795d71abee
Zarni Hpyoe <zarnihpyoe@gmail.com>
2017-10-30 14:33:54 -0400

    refactor: Move script to script.js

--- rankit/static/js/scripts.js
+++ rankit/static/js/scripts.js
@@ -1,1 +1,12 @@
 // Empty JS for your own code to be here
+const pool = document.querySelector('#top')
+const bot = document.querySelector('#bot')
+const containers = [
+  pool,
+  bot,
+]
+dragula(containers, {
+  copy: (el, source) => source === pool,
+  accepts: (el, target) => target !== pool,
+  removeOnSpill: true,
+})
