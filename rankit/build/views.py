9a546a9304e1f5e520b4ee18cd8c08bbd0cd9d22
Diana Doherty <sabitovadiana@gmail.com>
2017-11-14 16:28:32 -0500

    Update datasets

--- rankit/build/views.py
+++ rankit/build/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, url_for, make_response, jsonify, render_template, send_from_directory, redirect
 import os, json
 
-# import  rankit.build.rank_script.build_rank as build_rank
+import  rankit.build.rank_script.build_rank as build_rank
 import  pandas as pd
 
 build_blueprint = Blueprint(
