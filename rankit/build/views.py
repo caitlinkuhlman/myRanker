8b3af3e2e54a11051189c70d6cab61db4afb1e6e
Zarni Hpyoe <zarniphyo95@gmail.com>
2017-12-29 13:46:15 -0500

    Separate Build into 3 Methods (#44)

--- rankit/build/views.py
+++ rankit/build/views.py
@@ -40,6 +40,30 @@ def buildStart(dataset_name):
     return render_template('build.html', dataset_name = dataset_name, dataset=datastore_ids)
 
 
+@build_blueprint.route('/build/<dataset_name>/lc')
+def lc(dataset_name):
+
+    # retrieve dataset
+    datastore_ids = filterByPrimaryKey(getDataset(dataset_name))
+
+    return render_template('lc.html', dataset_name = dataset_name, dataset=datastore_ids)
+
+@build_blueprint.route('/build/<dataset_name>/cc')
+def cc(dataset_name):
+
+    # retrieve dataset
+    datastore_ids = filterByPrimaryKey(getDataset(dataset_name))
+
+    return render_template('cc.html', dataset_name = dataset_name, dataset=datastore_ids)
+
+@build_blueprint.route('/build/<dataset_name>/pwc')
+def pwc(dataset_name):
+
+    # retrieve dataset
+    datastore_ids = filterByPrimaryKey(getDataset(dataset_name))
+
+    return render_template('pwc.html', dataset_name = dataset_name, dataset=datastore_ids)
+
 @build_blueprint.route('/build/list/<dataset_name>')
 def listComparison(dataset_name):
 
