00775956a9c50e29b88fa3a540cc82bbae37cc9d
caitlinkuhlman <caitlinkuhlman@gmail.com>
2018-03-27 17:57:54 -0400

    build script

--- rankit/build/rank_script/build_rank.py
+++ rankit/build/rank_script/build_rank.py
@@ -12,7 +12,7 @@ def normalize(df):
 
 
 def scale(df):
-    return(100 * (df - df.min()) / (df.max() - df.min()))
+    return(df - df.min()) / (df.max() - df.min())
 
 # p_test -> pairs labeled 1 from training data
 # y_test -> labels
@@ -74,49 +74,6 @@ def get_confidence(data, clf):
         conf_scores[int(arr[i][2])] += arr[i][0]
     return conf_scores
 
-def build_with_conf(dataset, pairs, primary_key = 'Title', rank = 'Rank', score = 'Score', confd = 'Confidence') :
-
-#     make normalized copy of dataset
-    dataset_copy = dataset.copy(deep = True)
-    dataset_copy = clean_dataset(dataset_copy, primary_key)
-    dataset_copy.drop(primary_key, axis=1, inplace=True)
-
-#     get training pairs
-    X,y = get_training(dataset_copy,pairs)
-    data = np.array(dataset_copy)
-
-#     train linear SVM classifier
-    clf = SGDClassifier(penalty='L2',loss='hinge',fit_intercept=True,
-                        max_iter=5000,random_state=9)
-    clf.fit(X,y)
-
-    weights = clf.coef_[0]
-    y_pred=[]
-    y_pred=np.dot(weights,data.T)
-    conf = clf.decision_function(data)
-    p = int(len(X)/2)
-    p_test =X[0:p]
-    p_y = y[0:p]
-
-#     format output
-    weights_list = []
-    for i,w in enumerate(weights):
-        if w != 0.:
-            d={}
-            d['attribute']=dataset_copy.columns.values[i]
-            d['weight']=w
-            weights_list.append(d)
-
-    weights_json = json.dumps(weights_list)
-
-#     predicted score for each item
-    dataset[score] = y_pred
-#     ordinal ranking for each item
-    dataset[rank] = dataset[score].rank(ascending=False)
-#     confidence in prediction for each item
-    dataset[confd] = conf_scores
-
-    return dataset, weights
 
 def build(dataset, pairs, primary_key = 'Title', rank = 'Rank', score = 'Score', confd = 'Confidence') :
 
@@ -139,7 +96,7 @@ def build(dataset, pairs, primary_key = 'Title', rank = 'Rank', score = 'Score',
 #     not just previous input
 #     clf.partial_fit(X,y,np.unique(y))
     conf_scores = get_confidence(data, clf)
-    conf_scores = 100*scale(conf_scores)
+    conf_scores = scale(conf_scores)
 
     weights = clf.coef_[0]
     y_pred=[]
