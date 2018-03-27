c9532bf7701a11d78f8d00045e3790bc92dfefe2
caitlinkuhlman <caitlinkuhlman@gmail.com>
2018-03-27 16:43:42 -0400

    function to compute individual confidence scores

--- rankit/build/rank_script/build_rank.py
+++ rankit/build/rank_script/build_rank.py
@@ -54,6 +54,71 @@ def get_training(dataset,pairs):
         y.append(-1)
     return X,y
 
+def get_confidence(data, clf):
+    all_pairs = []
+    for i in range(len(data)):
+        for j in range(len(data)):
+            if i != j:
+                pair=[i,j]
+                d = data[i] - data[j]
+                all_pairs.append(np.append(d,pair))
+    df = pd.DataFrame()
+    df['conf'] = abs(clf.decision_function(np.array(all_pairs)[:,:-2]))
+    df['first'] = np.array(all_pairs)[:,-2]
+    df['second'] = np.array(all_pairs)[:,-1]
+    df.sort_values('conf', inplace=True)
+    arr = np.array(df)
+    conf_scores = np.zeros(len(data))
+    for i in range(len(arr)):
+        conf_scores[int(arr[i][1])] += arr[i][0]
+        conf_scores[int(arr[i][2])] += arr[i][0]
+    return conf_scores
+
+def build_with_conf(dataset, pairs, primary_key = 'Title', rank = 'Rank', score = 'Score', confd = 'Confidence') :
+
+#     make normalized copy of dataset
+    dataset_copy = dataset.copy(deep = True)
+    dataset_copy = clean_dataset(dataset_copy, primary_key)
+    dataset_copy.drop(primary_key, axis=1, inplace=True)
+    
+#     get training pairs
+    X,y = get_training(dataset_copy,pairs)
+    data = np.array(dataset_copy)
+    
+#     train linear SVM classifier
+    clf = SGDClassifier(penalty='L2',loss='hinge',fit_intercept=True,
+                        max_iter=5000,random_state=9)
+    clf.fit(X,y)
+ 
+    conf_scores = get_confidence(data, clf)
+    conf_scores = 100*scale(conf_scores)
+    weights = clf.coef_[0]
+    y_pred=[]
+    y_pred=np.dot(weights,data.T)
+    conf = clf.decision_function(data)
+    p = int(len(X)/2)
+    p_test =X[0:p]
+    p_y = y[0:p]
+
+#     format output
+    weights_list = []
+    for i,w in enumerate(weights):
+        if w != 0.:
+            d={}
+            d['attribute']=dataset_copy.columns.values[i]
+            d['weight']=w
+            weights_list.append(d)
+
+    weights_json = json.dumps(weights_list)
+    
+#     predicted score for each item
+    dataset[score] = y_pred
+#     ordinal ranking for each item
+    dataset[rank] = dataset[score].rank(ascending=False)
+#     confidence in prediction for each item
+    dataset[confd] = conf_scores
+
+    return dataset, weights
 
 def build(dataset, pairs, primary_key = 'Title', rank = 'Rank', score = 'Score', confd = 'Confidence') :
 
