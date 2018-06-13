0b3c788bb3b6b764dda72bb72bd49cfe4eb5c1aa
PAULHENRY <phscho33@gmail.com>
2018-06-12 20:11:33 -0400

    still has problems with confidence/score but it displays numbers

--- rankit/static/js/lib/dataTables.fixedColumns.js
+++ rankit/static/js/lib/dataTables.fixedColumns.js
@@ -523,7 +523,7 @@ $.extend( FixedColumns.prototype , {
 					mouseController = 'main';
 				}
 
-				if ( mouseController === 'main'|| mouseController === 'left') {
+				if ( mouseController === 'main') {
 					if ( that.s.iLeftColumns > 0 ) {
 						that.dom.grid.left.liner.scrollTop = that.dom.scroller.scrollTop;
 					}
